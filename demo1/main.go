505d9a756d6bcf078803ec1b12f4a997098a2148
kxn <163583@gmail.com>
2019-03-28 14:53:48 +0800

    modeif dep

--- demo1/main.go
+++ demo1/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"k8s-client/common"
 	"k8s.io/client-go/kubernetes"
 	core_v1 "k8s.io/api/core/v1"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"kube-client/common"
 )
 
 func main()  {
